fb66089ebcb7f56a1f96f0025e67e333ec557ed4
Molina <83372957+klaynmolina@users.noreply.github.com>
2023-08-28 17:44:55 -0300

    construtores

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -2,8 +2,16 @@ public class ContaTerminal {
 
 	private String nomeCliente;
 	private String agencia;
-	private int numero;
+	private Integer numero;
 	private Double saldo;
 
+	public ContaTerminal() {
+	}
 
+	public ContaTerminal(String nomeCliente, String agencia, Integer numero, Double saldo) {
+		this.nomeCliente = nomeCliente;
+		this.agencia = agencia;
+		this.numero = numero;
+		this.saldo = saldo;
+	}
 }
