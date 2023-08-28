c4563e2b735dc4c2c74a39f5d33670aa7e54b26e
Molina <83372957+klaynmolina@users.noreply.github.com>
2023-08-28 17:46:03 -0300

    getters e setters

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -14,4 +14,37 @@ public class ContaTerminal {
 		this.numero = numero;
 		this.saldo = saldo;
 	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+			
 }
